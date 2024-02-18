b176b5e905539db7d65bd367afd7a56b32ce3398
sergo <sergo@sergo>
2024-02-18 23:02:43 +0300

    for with range

---
+++ print-args-cmd-4-for-range.go
@@ -0,0 +1,16 @@
+// Print args from cmd
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
