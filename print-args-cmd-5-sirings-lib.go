8aabcf38645d9e0e7baefb234ea3d2501541ce82
sergo <sergo@sergo>
2024-02-24 20:45:57 +0300

    strings.Builder

--- print-args-cmd-5-sirings-lib.go
+++ print-args-cmd-5-sirings-lib.go
@@ -8,5 +8,13 @@ import (
 )
 
 func main() {
+	// way 1 - disadvantages: allocations of strings, due to strings are immutable
 	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	// way 2 - advantage: NO allocations of strings
+	var stringBuilder strings.Builder
+	for _, currentString := range os.Args[1:] {
+		stringBuilder.WriteString(currentString)
+	}
+	fmt.Println(stringBuilder.String())
 }
