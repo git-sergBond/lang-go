bd2d4b6c23b81474475533a54bfaf2487718a007
sergo <sergo@sergo>
2024-02-24 21:11:46 +0300

    simple way

--- print-args-cmd-5-sirings-lib.go
+++ print-args-cmd-5-sirings-lib.go
@@ -8,14 +8,34 @@ import (
 )
 
 func main() {
+	// way 0
+	fmt.Println("-------")
+	fmt.Println("way 0")
+	fmt.Println(os.Args[1:])
+	fmt.Println("-------")
+
 	// way 1 - disadvantages: allocations of strings, due to strings are immutable
+	fmt.Println("way 1")
 	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println("-------")
 
 	// way 2 - advantage: NO allocations of strings
+	fmt.Println("way 2")
 	var stringBuilder strings.Builder
 	for _, currentString := range os.Args[1:] {
 		stringBuilder.WriteString(currentString)
 		stringBuilder.WriteString(" ")
 	}
 	fmt.Println(stringBuilder.String())
+	fmt.Println("-------")
+
+	// way 3 - advantage: NO allocations of strings, code style like in documentation
+	/*
+		var stringBuilder2 strings.Builder
+		stringBuilder2.Grow(1000)
+		for _, currentString := range os.Args[1:] {
+			fmt.Fprintf(&stringBuilder2, "%d", currentString)
+		}
+		fmt.Println(stringBuilder2.String())
+	*/
 }
