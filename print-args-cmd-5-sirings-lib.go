fedf46cb645dce5c0c227e9ed8bbbab48879fe02
sergo <sergo@sergo>
2024-02-24 21:23:29 +0300

    split to functions

--- print-args-cmd-5-sirings-lib.go
+++ print-args-cmd-5-sirings-lib.go
@@ -8,37 +8,49 @@ import (
 )
 
 func main() {
-	// way 0
-	fmt.Println("-------")
-	fmt.Println("way 0")
+	// advantage: simple
+	fmt.Println("---simpleWay----")
+	simpleWay()
+
+	// disadvantages: allocations of strings, due to strings are immutable
+	fmt.Println("---joinWay----")
+	joinWay()
+
+	// advantage: NO allocations of strings
+	fmt.Println("---stringBuilderWay----")
+	stringBuilderWay()
+
+	// advantage: NO allocations of strings, code style like in documentation
+	fmt.Println("---stringBuilderFprintfWay----")
+	stringBuilderFprintfWay()
+}
+
+func simpleWay() {
 	fmt.Println(os.Args[1:])
-	fmt.Println("-------")
+}
 
-	// way 1 - disadvantages: allocations of strings, due to strings are immutable
-	fmt.Println("way 1")
+func joinWay() {
 	fmt.Println(strings.Join(os.Args[1:], " "))
-	fmt.Println("-------")
+}
 
-	// way 2 - advantage: NO allocations of strings
-	fmt.Println("way 2")
+func stringBuilderWay() {
 	var stringBuilder strings.Builder
 	for _, currentString := range os.Args[1:] {
 		stringBuilder.WriteString(currentString)
 		stringBuilder.WriteString(" ")
 	}
 	fmt.Println(stringBuilder.String())
-	fmt.Println("-------")
+}
 
-	// way 3 - advantage: NO allocations of strings, code style like in documentation
-	fmt.Println("way 3")
-	var stringBuilder2 strings.Builder
-	stringBuilder2.Grow(1000)
+func stringBuilderFprintfWay() {
+	var stringBuilder strings.Builder
+	stringBuilder.Grow(32)
 	for _, currentString := range os.Args[1:] {
-		_, err := fmt.Fprintf(&stringBuilder2, "%s ", currentString)
+		_, err := fmt.Fprintf(&stringBuilder, "%s ", currentString)
 		if err != nil {
+			fmt.Println("ERROR: main. strings.Builder + fmt.Fprintf")
 			return
 		}
 	}
-	fmt.Println(stringBuilder2.String())
-	fmt.Println("-------")
+	fmt.Println(stringBuilder.String())
 }
