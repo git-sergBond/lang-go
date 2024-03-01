f9db6cb82dc9e5b056ce779bf72e9e85f8048569
sergo <sergo@sergo>
2024-03-01 10:03:45 +0300

    fetch.go - v2

---
+++ examples/fetch/fetch_v2.go
@@ -0,0 +1,43 @@
+package fetch
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func FetchV2() {
+	//read command line arguments
+	urlsFlag := flag.String("urls", "", "https://www.google.ru,https://dribbble.com/tags/simple-website")
+	flag.Parse()
+
+	//parse -urls flag
+	urls := strings.Split(*urlsFlag, ",")
+	log.Printf("urls: %v", urls)
+
+	countUrls := len(urls)
+	for i, url := range urls {
+		log.Println()
+		log.Printf("url[%d/%d]: %s", i+1, countUrls, url)
+
+		// fetch http resource by URL
+		response, respErr := http.Get(url)
+		if respErr != nil {
+			log.Fatalf("fetch/http.Get: %s %v", url, respErr)
+		}
+
+		// print body
+		_, copyError := io.Copy(os.Stdout, response.Body)
+		if copyError != nil {
+			log.Fatalf("fetch/response.Body.Close: %v", copyError)
+		}
+
+		closeErr := response.Body.Close()
+		if closeErr != nil {
+			log.Fatalf("fetch/response.Body.Close: %v", closeErr)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 
 func main() {
 	//place here what are you wanting to run
-	fetch.Fetch()
+	fetch.FetchV2()
 }
