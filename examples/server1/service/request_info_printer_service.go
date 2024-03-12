35b047b36646ecc9ae85304a75a5666adc48d000
sergo <sergo@sergo>
2024-03-12 23:47:16 +0300

    server_minimal.go - refactor

--- examples/server1/service/request_info_printer_service.go
+++ examples/server1/service/request_info_printer_service.go
@@ -9,37 +9,32 @@ import (
 
 func PrintRequestInfo(writer io.Writer, request *http.Request) {
 	// API info
-	_, err := fmt.Fprintf(writer, "URL.Path = %q", request.URL.Path)
-	if err != nil {
-		printRequestInfoError("request.URL.Path", request)
+	if _, err := fmt.Fprintf(writer, "URL.Path = %q", request.URL.Path); err != nil {
+		FprintfError("request.URL.Path", request, err)
 	}
-	_, err = fmt.Fprintf(writer, "%s %s %s\n", request.Method, request.URL, request.Proto)
-	if err != nil {
-		printRequestInfoError("request.Method, request.URL, request.Proto", request)
+	if _, err := fmt.Fprintf(writer, "%s %s %s\n", request.Method, request.URL, request.Proto); err != nil {
+		FprintfError("request.Method, request.URL, request.Proto", request, err)
 	}
 	// headers
 	for header, value := range request.Header {
-		_, err := fmt.Fprintf(writer, "header[%q] = %q\n", header, value)
-		if err != nil {
-			printRequestInfoError("request.Header[header], request.Header[value]", request)
+		if _, err := fmt.Fprintf(writer, "header[%q] = %q\n", header, value); err != nil {
+			FprintfError("request.Header[header], request.Header[value]", request, err)
 		}
 	}
-	_, err = fmt.Fprintf(writer, "Host = %q\nRemoteAddr = %q\n", request.Host, request.RemoteAddr)
-	if err != nil {
-		printRequestInfoError("request.Host, request.RemoteAddr", request)
+	if _, err := fmt.Fprintf(writer, "Host = %q\nRemoteAddr = %q\n", request.Host, request.RemoteAddr); err != nil {
+		FprintfError("request.Host, request.RemoteAddr", request, err)
 	}
 	// print form data
 	if err := request.ParseForm(); err != nil {
 		log.Printf("ERROR: in request.ParseForm() err = %v", err)
 	}
 	for field, value := range request.Form {
-		_, err := fmt.Fprintf(writer, "Form[%q] = %q\n", field, value)
-		if err != nil {
-			printRequestInfoError("request.Form[key], request.Form[value]", request)
+		if _, err := fmt.Fprintf(writer, "Form[%q] = %q\n", field, value); err != nil {
+			FprintfError("request.Form[key], request.Form[value]", request, err)
 		}
 	}
 }
 
-func printRequestInfoError(arguments string, request *http.Request) {
-	log.Printf("ERROR: in fmt.Fprintf(%s) with request = %v", arguments, request)
+func FprintfError(arguments string, request *http.Request, err error) {
+	log.Printf("ERROR: in fmt.Fprintf(%s) with request = %v\n ERROR DETAILS: %v", arguments, request, err)
 }
