7ecaf8bd6a7065def91c7bf954c9be6d54ec98f0
sergo <sergo@sergo>
2024-03-12 13:55:11 +0300

    server_minimal.go - add request counter

--- examples/server1/server_minimal.go
+++ examples/server1/server_minimal.go
@@ -6,16 +6,39 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+var countRequests int64 = 0
+var mutexForCountRequests sync.Mutex
 
 func ServerMinimal() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", countHandler)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	_, err := fmt.Fprintf(writer, "URL.Path = %q", request.URL.Path)
+// Print request path
+func handler(response http.ResponseWriter, request *http.Request) {
+	incrementCounter(request)
+	_, err := fmt.Fprintf(response, "URL.Path = %q", request.URL.Path)
 	if err != nil {
 		log.Printf("ERROR: in handler/Fprintf with request.URL.Path argument = " + request.URL.Path)
 	}
+}
+
+// Print number of requests
+func countHandler(response http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(response, "%d", countRequests)
+	if err != nil {
+		log.Printf("ERROR: in handler/countHandler with countRequests = %d", countRequests)
+	}
+}
+
+func incrementCounter(request *http.Request) {
+	log.Printf("DEBUG: reqest = %v", request)
+	mutexForCountRequests.Lock()
+	countRequests++
+	log.Printf("INFO: countRequests = %d", countRequests)
+	mutexForCountRequests.Unlock()
 }
