952c40b8047c18ebd4f9db3219cfdc0d9dd91b27
sergo <sergo@sergo>
2024-03-13 00:09:57 +0300

    server_minimal.go - Two ways define anonymous functions

--- examples/server1/server_minimal.go
+++ examples/server1/server_minimal.go
@@ -3,6 +3,7 @@
 package server1
 
 import (
+	"example.com/examples/lissajous"
 	"example.com/examples/server1/resource"
 	"log"
 	"net/http"
@@ -12,5 +13,16 @@ func ServerMinimal() {
 	http.HandleFunc("/", resource.RootHandler)
 	http.HandleFunc("/count", resource.GetCountRequestsHandler)
 	http.HandleFunc("/gif", resource.GifExampleHandler)
+
+	// Two ways define anonymous functions
+	http.HandleFunc("/gif1", func(writer http.ResponseWriter, request *http.Request) {
+		lissajous.LissajousGif(writer)
+	})
+
+	handler := func(writer http.ResponseWriter, request *http.Request) {
+		lissajous.LissajousGif(writer)
+	}
+	http.HandleFunc("/gif2", handler)
+
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
