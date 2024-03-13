fff1583d8051e732221c258b8c57c7e3a6bf2a1d
sergo <sergo@sergo>
2024-03-14 00:16:31 +0300

    server_minimal.go - refactor - naming

--- examples/lissajous/lissajous.go
+++ examples/lissajous/lissajous.go
@@ -43,38 +43,38 @@ func Lissajous() {
 func LissajousGif(out io.Writer, settings map[string]any) {
 	// constants for GIF
 	const (
-		cycles  = 5     // Количество полных колебаний
-		res     = 0.001 // Угловое разрешение
-		size    = 100   // Канва изображения охватывает [size..+size]
-		nframes = 64    // Количество кадров анимации
-		delay   = 8     // Задержка между кадрами
+		_cycles  = 5     // Количество полных колебаний
+		_res     = 0.001 // Угловое разрешение
+		_size    = 100   // Канва изображения охватывает [_size..+_size]
+		_nFrames = 64    // Количество кадров анимации
+		_delay   = 8     // Задержка между кадрами
 	)
 
 	// override settings
-	var cyclesVar = float64(cycles)
+	var cycles = float64(_cycles)
 	if len(settings) > 0 {
 		if val, ok := settings["cycles"].(int); ok {
-			cyclesVar = float64(val)
+			cycles = float64(val)
 		}
 	}
-	log.Printf("DEBUG: cyclesVar=%v", cyclesVar)
+	log.Printf("DEBUG: cycles=%v", cycles)
 
 	// variables for gradient
 	var lenPalette = uint8(len(palette)) - 1
 	var lineColorIndex uint8 = 1
-	gradientSwitchAfterFrames := nframes / lenPalette
+	gradientSwitchAfterFrames := _nFrames / lenPalette
 
 	// variables for figure
 	r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 	freq := r.Float64() * 3.0 // Относительная частота колебаний y
-	anim := gif.GIF{LoopCount: nframes}
+	anim := gif.GIF{LoopCount: _nFrames}
 	phase := 0.0 // разность фаз
 
 	// algorithm draw frames for GIF
-	for frame := 0; frame < nframes; frame++ {
+	for frame := 0; frame < _nFrames; frame++ {
 
 		// create new empty frame
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		rect := image.Rect(0, 0, 2*_size+1, 2*_size+1)
 		img := image.NewPaletted(rect, palette)
 
 		// algorithm draw for gradient
@@ -87,15 +87,15 @@ func LissajousGif(out io.Writer, settings map[string]any) {
 		}
 
 		// algorithm draw for figure
-		for t := 0.0; t < cyclesVar*2*math.Pi; t += res {
+		for t := 0.0; t < cycles*2*math.Pi; t += _res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), lineColorIndex)
+			img.SetColorIndex(_size+int(x*_size+0.5), _size+int(y*_size+0.5), lineColorIndex)
 		}
 
 		// create animation
 		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
+		anim.Delay = append(anim.Delay, _delay)
 		anim.Image = append(anim.Image, img)
 	}
 
