10e70e06890fc65929ff046123c414a5a38f918e
sergo <sergo@sergo>
2024-03-27 23:33:17 +0300

    tmp

--- examples/structs/anonymous/struct_with_func2.go
+++ examples/structs/anonymous/struct_with_func2.go
@@ -1,18 +1,37 @@
+/*
+НЕ ПОЛУЧИЛСЯ ПРИМЕР ОТ СЮДА https://www.kelche.co/blog/go/golang-anonymous-struct/#anonymous-struct-interface-in-golang
+- Попробовать стапрую версию Go раннее 17 Jan 2023
+*/
 package anonymous
 
 /*
+import "fmt"
+
+type person interface {
+	greet()
+}
+
+func (s struct {
+	name string
+	age  int
+}) greet() {
+	fmt.Println("Hello", s.name)
+}
+
+func SayHello(p person) {
+
+	p.greet()
+}
+
 func ExampleAnonStructWithFunc2() {
-	anonymousStructWithMethod := struct {
-		firstName, lastName, patronymic string
+	s1 := struct {
+		name string
+		age  int
 	}{
-		firstName:  "Sergey",
-		lastName:   "Bondarenko",
-		patronymic: "Sergeevich",
+		name: "John",
+		age:  30,
 	}
 
-	//anonymousStructWithMethod.Greeting()
+	s1.greet()
 }
 */
-//func (s struct{ firstName, lastName, patronymic string }) Greeting() {
-//	fmt.Println(s)
-//}
