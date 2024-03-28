ddac110b8c49264ea88a444c7a88129ddd0d78cd
sergo <sergo@sergo>
2024-03-28 20:33:45 +0300

    methods_example.go - mutability 2

--- examples/methods/methods_example.go
+++ examples/methods/methods_example.go
@@ -22,28 +22,46 @@ type t1 struct {
 	t int
 }
 
-func (tx t1) funcT1(t int) {
+func (tx t1) immutableSetter(t int) {
 	tx.t = t
-	fmt.Println("non mutable f1:", tx.t)
+	fmt.Println("immutableSetter:", tx.t)
 }
-func (tx *t1) funcT2(t int) {
+func (tx *t1) mutableSetter(t int) {
 	tx.t = t
-	fmt.Println("mutable f2:", tx.t)
+	fmt.Println("mutableSetter:", tx.t)
+}
+
+func (tx *t1) mutableIncrement1() {
+	tx.t = tx.t + 1
+	fmt.Println("mutableUpdate1:", tx.t)
+}
+
+func (tx *t1) mutableIncrement2() {
+	(*tx).t = (*tx).t + 1
+	fmt.Println("mutableUpdate2:", tx.t)
 }
 
 // Ex2
 func MethodMutability() {
 	_t1 := t1{t: 0}
 
-	_t1.funcT1(1)
-	fmt.Println("non mutable result:", _t1)
+	_t1.immutableSetter(1)
+	fmt.Println("1 result:", _t1)
 
-	(&_t1).funcT1(2)
-	fmt.Println("non mutable result:", _t1)
+	(&_t1).immutableSetter(2)
+	fmt.Println("2 result:", _t1)
 
-	_t1.funcT2(3)
-	fmt.Println("mutable result:", _t1)
+	_t1.mutableSetter(3)
+	fmt.Println("3 result:", _t1)
 
-	(&_t1).funcT2(4)
-	fmt.Println("mutable result:", _t1)
+	(&_t1).mutableSetter(4)
+	fmt.Println("4 result:", _t1)
+
+	t1Link := &_t1
+
+	t1Link.mutableIncrement1()
+	fmt.Println("5 result:", _t1)
+
+	t1Link.mutableIncrement2()
+	fmt.Println("6 result:", _t1)
 }
